04482a38400717f8ce651c4dbde0338d915c69d9
jessexknight <jesse.x.knight@protonmail.com>
2024-05-16 11:27:26 +0100

    code: update profile: add e vs l

--- code/profile/run.sh
+++ code/profile/run.sh
@@ -1,5 +1,8 @@
-# WARNING: we modify original files: don't tinker with the 's/find/replace/'!
+# by code line (inaccurate)
 for py in model/*.py; do sed -i 's/\#\@profile/\@profile/' $py; done
-(date; kernprof -lv -u 1 profile/run.py) > profile/profile.txt
+(date; kernprof -lv -u 1 profile/run.py) > profile/profile.l.txt
 for py in model/*.py; do sed -i 's/\@profile/\#\@profile/' $py; done
 rm run.py.lprof
+# by code element (accurate)
+(date; kernprof -v -u 1 profile/run.py) > profile/profile.e.txt
+rm run.py.prof
