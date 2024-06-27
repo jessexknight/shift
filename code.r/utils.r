49dd3dbc8b0a417c1e8a41a2a01a80864853edb1
jessexknight <jesse.x.knight@protonmail.com>
2024-06-26 22:54:42 -0700

    code: add root.path

--- code.r/utils.r
+++ code.r/utils.r
@@ -1,6 +1,17 @@
 len = length
 lens = lengths
 
+# proj.root: full path to parent of /code.r/fio.r
+proj.root = strsplit(file.path(getwd(),''),file.path('','code.r',''))[[1]][1]
+
+root.path = function(...,create=FALSE){
+  # e.g. root.path('abc','123') returns proj.root/abc/123
+  path = file.path(proj.root,...)
+  if (create & !dir.exists(dirname(path))){
+    dir.create(dirname(path),recursive=TRUE) }
+  return(path)
+}
+
 even.len = function(x){
   # truncate vector x to have an even length
   length(x) = len(x) - (len(x) %% 2)
@@ -10,9 +21,4 @@ even.len = function(x){
 last = function(x){
   # get the last element in x or NA if len(x) == 0
   ifelse(len(x),tail(x,1),NA)
-}
-
-alen = function(x){
-  # check if len(x) > 0
-  len(x) > 0
 }
