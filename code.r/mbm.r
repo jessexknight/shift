dc73d73293242637a7a08bf7982c0f5ef28a1b9c
jessexknight <jesse.x.knight@protonmail.com>
2024-07-04 15:00:13 -0400

    code: add mbm (microbenchmark)

---
+++ code.r/mbm.r
@@ -0,0 +1,17 @@
+source('sim.r')
+options(width=256)
+
+N = list(rep=3,seed=7)
+f = file.path('.tmp','mbm',sprintf('mbm_s%dr%d_%s.out',
+  N$seed,N$rep,Sys.Date()))
+
+mbm = microbenchmark::microbenchmark(
+  {sim.runs(lapply(1:N$seed,get.pars,n= 30))},
+  {sim.runs(lapply(1:N$seed,get.pars,n=100))},
+  {sim.runs(lapply(1:N$seed,get.pars,n=300))},
+times=N$rep)
+
+print(mbm)
+sink(f)
+print(mbm)
+sink()
