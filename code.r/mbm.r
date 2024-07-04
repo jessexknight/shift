d3a7eeef703013a01bc72208e1fa7c2dee1ffe6d
jessexknight <jesse.x.knight@protonmail.com>
2024-07-04 18:07:14 -0400

    code: update mbm

--- code.r/mbm.r
+++ code.r/mbm.r
@@ -6,9 +6,10 @@ f = file.path('.tmp','mbm',sprintf('mbm_s%dr%d_%s.out',
   N$seed,N$rep,Sys.Date()))
 
 mbm = microbenchmark::microbenchmark(
-  {sim.runs(lapply(1:N$seed,get.pars,n= 30))},
+  {sim.runs(lapply(1:N$seed,get.pars,n=30))},
   {sim.runs(lapply(1:N$seed,get.pars,n=100))},
   {sim.runs(lapply(1:N$seed,get.pars,n=300))},
+  {sim.runs(lapply(1:N$seed,get.pars,n=1000))},
 times=N$rep)
 
 print(mbm)
