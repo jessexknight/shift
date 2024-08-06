677dd63825319e9a8faf9554eba691ff27963018
jessexknight <jesse.x.knight@protonmail.com>
2024-08-06 11:32:43 +0100

    big rename Is -> I, Es -> E, Ks -> K

--- code.r/sim.r
+++ code.r/sim.r
@@ -2,10 +2,10 @@
 # =============================================================================
 # initialization funs
 
-init.evts = function(Is){
+init.evts = function(I){
   # initialize event vectors for each event type & individual
-  E = lapply(Is$i,function(i){ numeric() })
-  Es = lapply(evts,function(e){ E })
+  E0 = lapply(I,function(i){ numeric() })
+  E  = lapply(evts,function(e){ E0 })
 }
 
 init.inds = function(P){
@@ -32,7 +32,7 @@ init.inds = function(P){
   # for (i in 1:3) plot(ptr[,-i],col=rgb(0,0,0,.1)) # DEBUG
   # create main df of individuals ---------------------------------------------
   age = runif(n,min=amin-P$ndur*adur,max=amax)
-  Is = data.frame(
+  I = data.frame(
     i = seq(n),
     z.born  = -age*z1y,
     age     = +age,
@@ -62,16 +62,16 @@ init.inds = function(P){
   )
 }
 
-init.ptrs = function(P,Is,i,z){
-  # generate partners among Is
+init.ptrs = function(P,I,i,z){
+  # generate partners among I
   # mixing is random by splitting into 1st/2nd half
   n = len(i)/2
   if (n==0){ return(NULL) }
   i1 = i[0+(1:n)]
   i2 = i[n+(1:n)]
-  Ks = data.frame(
-    i1 = Is$i[i1], # i of partner 1
-    i2 = Is$i[i2], # i of partner 2
+  K = data.frame(
+    i1 = I$i[i1], # i of partner 1
+    i2 = I$i[i2], # i of partner 2
     zo = z # timestep ptr starts
     # TODO: f.sex, p.cdm
   )
@@ -84,69 +84,69 @@ rate.to.prob = function(R){ p = 1-exp(-R*dtz) }
 rate.to.bool = function(R){ b = runif(R) < (1-exp(-R*dtz)) }
 rate.to.num  = function(R){ n = rpois(len(R),R*dtz) }
 
-rate.vio = function(P,Js,aj){
+rate.vio = function(P,J,aj){
   R = ( # among all
-      Js$vio.Ri # base rate
+      J$vio.Ri # base rate
     * P$aRR.vio[aj] # RR age
 ); return(R) }
 
-rate.dep_o = function(P,Js,R,aj,z){
-  j = which(!Js$dep.now)
+rate.dep_o = function(P,J,R,aj,z){
+  j = which(!J$dep.now)
   R[j] = ( # among not dep
-      Js$dep_o.Ri[j] # base rate
+      J$dep_o.Ri[j] # base rate
     * P$aRR.dep_o[aj[j]] # RR age
-    * (1 + P$RRu.dep_o.dep_p * Js$dep.past[j]) # RR dep past
-    * map.tRR(P$tRRu.dep_o.vio_z,Js$vio.zf[j],z) # tRR vio
-    * P$nRR.dep_o.vio_n[Js$vio.n[j]+1] # nRR vio
+    * (1 + P$RRu.dep_o.dep_p * J$dep.past[j]) # RR dep past
+    * map.tRR(P$tRRu.dep_o.vio_z,J$vio.zf[j],z) # tRR vio
+    * P$nRR.dep_o.vio_n[J$vio.n[j]+1] # nRR vio
 ); return(R) }
 
-rate.dep_x = function(P,Js,R,aj,z){
-  j = which(Js$dep.now)
+rate.dep_x = function(P,J,R,aj,z){
+  j = which(J$dep.now)
   R[j] = ( # among dep
-      Js$dep_x.Ri[j] # base rate
-    * map.tRR(P$dRRu.dep_x.dep_u,Js$dep.zo[j],z) # RR dep dur
-    * map.tRR(P$tRRu.dep_x.vio_z,Js$vio.zf[j],z) # tRR vio
+      J$dep_x.Ri[j] # base rate
+    * map.tRR(P$dRRu.dep_x.dep_u,J$dep.zo[j],z) # RR dep dur
+    * map.tRR(P$tRRu.dep_x.vio_z,J$vio.zf[j],z) # tRR vio
 ); return(R) }
 
-rate.haz_o = function(P,Js,R,aj,z){
-  j = which(!Js$haz.now)
+rate.haz_o = function(P,J,R,aj,z){
+  j = which(!J$haz.now)
   R[j] = ( # among not haz
-      Js$haz_o.Ri[j] # base rate
+      J$haz_o.Ri[j] # base rate
     * P$aRR.haz_o[aj[j]] # RR age
-    * (1 + P$RRu.haz_o.haz_p * Js$haz.past[j]) # RR haz past
-    * (1 + P$RRu.haz_o.dep_w * Js$dep.now[j]) # RR dep now
-    * map.tRR(P$tRRu.haz_o.vio_z,Js$vio.zf[j],z) # tRR vio
-    * P$nRR.haz_o.vio_n[Js$vio.n[j]+1] # nRR vio
+    * (1 + P$RRu.haz_o.haz_p * J$haz.past[j]) # RR haz past
+    * (1 + P$RRu.haz_o.dep_w * J$dep.now[j]) # RR dep now
+    * map.tRR(P$tRRu.haz_o.vio_z,J$vio.zf[j],z) # tRR vio
+    * P$nRR.haz_o.vio_n[J$vio.n[j]+1] # nRR vio
 ); return(R) }
 
-rate.haz_x = function(P,Js,R,aj,z){
-  j = which(Js$haz.now)
+rate.haz_x = function(P,J,R,aj,z){
+  j = which(J$haz.now)
   R[j] = ( # among haz
-      Js$haz_x.Ri[j] # base rate
-    * map.tRR(P$dRRu.haz_x.haz_u,Js$haz.zo[j],z) # RR haz dur
-    * (1 + P$RRu.haz_x.dep_w * Js$dep.now[j]) # RR dep now
-    * map.tRR(P$tRRu.haz_x.vio_z,Js$vio.zf[j],z) # tRR vio
+      J$haz_x.Ri[j] # base rate
+    * map.tRR(P$dRRu.haz_x.haz_u,J$haz.zo[j],z) # RR haz dur
+    * (1 + P$RRu.haz_x.dep_w * J$dep.now[j]) # RR dep now
+    * map.tRR(P$tRRu.haz_x.vio_z,J$vio.zf[j],z) # tRR vio
 ); return(R) }
 
-rate.ptr_o = function(P,Js,R,aj,z){
-  j = which(Js$age > Js$age.act & Js$ptr.n < Js$ptr.max)
+rate.ptr_o = function(P,J,R,aj,z){
+  j = which(J$age > J$age.act & J$ptr.n < J$ptr.max)
   R[j] = ( # among sex active & avail
-      Js$ptr_o.Ri[j] # base rate
+      J$ptr_o.Ri[j] # base rate
     * P$aRR.ptr_o[aj[j]] # RR age
-    * (1 + P$RRu.ptr_o.dep_w * Js$dep.now[j]) # RR dep now
-    * (1 + P$RRu.ptr_o.haz_w * Js$haz.now[j]) # RR haz now
-    * map.tRR(P$tRRu.ptr_o.vio_z,Js$vio.zf[j],z) # tRR vio
-    * P$nRR.ptr_o.vio_n[Js$vio.n[j]+1] # nRR vio
+    * (1 + P$RRu.ptr_o.dep_w * J$dep.now[j]) # RR dep now
+    * (1 + P$RRu.ptr_o.haz_w * J$haz.now[j]) # RR haz now
+    * map.tRR(P$tRRu.ptr_o.vio_z,J$vio.zf[j],z) # tRR vio
+    * P$nRR.ptr_o.vio_n[J$vio.n[j]+1] # nRR vio
 ); return(R) }
 
-rate.ptr_x = function(P,Ks,Is){
-  i1 = Ks$i1
-  i2 = Ks$i2
+rate.ptr_x = function(P,K,I){
+  i1 = K$i1
+  i2 = K$i2
   R = ( # among all (ptrs)
-      0.5 * (Is$ptr_x.Ri[i1] + Is$ptr_x.Ri[i2])
+      0.5 * (I$ptr_x.Ri[i1] + I$ptr_x.Ri[i2])
     # TODO: RR age
-    * (1 + P$RRu.ptr_x.dep_w * (Is$dep.now[i1] + Is$dep.now[i2])) # RR dep now
-    * (1 + P$RRu.ptr_x.haz_w * (Is$haz.now[i1] + Is$haz.now[i2])) # RR haz now
+    * (1 + P$RRu.ptr_x.dep_w * (I$dep.now[i1] + I$dep.now[i2])) # RR dep now
+    * (1 + P$RRu.ptr_x.haz_w * (I$haz.now[i1] + I$haz.now[i2])) # RR haz now
 ); return(R) }
 
 # =============================================================================
@@ -155,69 +155,69 @@ rate.ptr_x = function(P,Ks,Is){
 sim.run = function(P,rm.dum=TRUE){
   # initialization ------------------------------------------------------------
   set.seed(P$seed)
-  Is = init.inds(P)  # individuals
-  Es = init.evts(Is) # events
-  Ks = NULL          # partnerships
+  I = init.inds(P) # individuals
+  E = init.evts(P) # events
+  K = NULL         # partnerships
   # event loop ----------------------------------------------------------------
   for (z in 1:P$zf){
     # if (z %% z1y == 0) { print(z/z1y) } # DEBUG
     # age inds ----------------------------------------------------------------
-    Is$age = Is$age + 1/z1y
+    I$age = I$age + 1/z1y
     # ptr dissolve ------------------------------------------------------------
-    b = rate.to.bool(rate.ptr_x(P,Ks,Is))
-    i = c(Ks$i1[b],Ks$i2[b])
-    Is$ptr.n[i] = Is$ptr.n[i] - 1
-    Es$ptr_x[i] = lapply(Es$ptr_x[i],append,z)
-    Ks = Ks[!b,]
+    b = rate.to.bool(rate.ptr_x(P,K,I))
+    i = c(K$i1[b],K$i2[b])
+    I$ptr.n[i] = I$ptr.n[i] - 1
+    E$ptr_x[i] = lapply(E$ptr_x[i],append,z)
+    K = K[!b,]
     # select active inds ------------------------------------------------------
-    i = which(Is$age > amin & Is$age < amax)
-    Js = Is[i,] # read only copy of active
-    ij = match(Js$i,Is$i) # map j -> j
-    aj = floor(Js$age-amin+1) # age vector for j
-    R0 = numeric(nrow(Js)) # init rate = 0 for j
+    i = which(I$age > amin & I$age < amax)
+    J = I[i,] # read only copy of active
+    ij = match(J$i,I$i) # map j -> j
+    aj = floor(J$age-amin+1) # age vector for j
+    R0 = numeric(nrow(J)) # init rate = 0 for j
     # update vio events -------------------------------------------------------
-    i = ij[which(rate.to.bool(rate.vio(P,Js,aj)))]
-    Is$vio.zf[i] = z
-    Is$vio.n[i] = Is$vio.n[i] + 1
-    Es$vio[i] = lapply(Es$vio[i],append,z)
+    i = ij[which(rate.to.bool(rate.vio(P,J,aj)))]
+    I$vio.zf[i] = z
+    I$vio.n[i] = I$vio.n[i] + 1
+    E$vio[i] = lapply(E$vio[i],append,z)
     # update dep onset --------------------------------------------------------
-    i = ij[which(rate.to.bool(rate.dep_o(P,Js,R0,aj,z)))]
-    Is$dep.now[i] = TRUE
-    Is$dep.past[i] = TRUE
-    Is$dep.zo[i] = z
-    Es$dep_o[i] = lapply(Es$dep_o[i],append,z)
+    i = ij[which(rate.to.bool(rate.dep_o(P,J,R0,aj,z)))]
+    I$dep.now[i] = TRUE
+    I$dep.past[i] = TRUE
+    I$dep.zo[i] = z
+    E$dep_o[i] = lapply(E$dep_o[i],append,z)
     # update dep recovery -----------------------------------------------------
-    i = ij[which(rate.to.bool(rate.dep_x(P,Js,R0,aj,z)))]
-    Is$dep.now[i] = FALSE
-    Es$dep_x[i] = lapply(Es$dep_x[i],append,z)
+    i = ij[which(rate.to.bool(rate.dep_x(P,J,R0,aj,z)))]
+    I$dep.now[i] = FALSE
+    E$dep_x[i] = lapply(E$dep_x[i],append,z)
     # update haz onset --------------------------------------------------------
-    i = ij[which(rate.to.bool(rate.haz_o(P,Js,R0,aj,z)))]
-    Is$haz.now[i] = TRUE
-    Is$haz.past[i] = TRUE
-    Is$haz.zo[i] = z
-    Es$haz_o[i] = lapply(Es$haz_o[i],append,z)
+    i = ij[which(rate.to.bool(rate.haz_o(P,J,R0,aj,z)))]
+    I$haz.now[i] = TRUE
+    I$haz.past[i] = TRUE
+    I$haz.zo[i] = z
+    E$haz_o[i] = lapply(E$haz_o[i],append,z)
     # update haz recovery -----------------------------------------------------
-    i = ij[which(rate.to.bool(rate.haz_x(P,Js,R0,aj,z)))]
-    Is$haz.now[i] = FALSE
-    Es$haz_x[i] = lapply(Es$haz_x[i],append,z)
+    i = ij[which(rate.to.bool(rate.haz_x(P,J,R0,aj,z)))]
+    I$haz.now[i] = FALSE
+    E$haz_x[i] = lapply(E$haz_x[i],append,z)
     # ptr formation -----------------------------------------------------------
-    i = ij[even.len(which(rate.to.bool(rate.ptr_o(P,Js,R0,aj,z))))]
-    Is$ptr.n[i] = Is$ptr.n[i] + 1
-    Es$ptr_o[i] = lapply(Es$ptr_o[i],append,z)
-    Ks = rbind(Ks,init.ptrs(P,Is,i,z))
+    i = ij[even.len(which(rate.to.bool(rate.ptr_o(P,J,R0,aj,z))))]
+    I$ptr.n[i] = I$ptr.n[i] + 1
+    E$ptr_o[i] = lapply(E$ptr_o[i],append,z)
+    K = rbind(K,init.ptrs(P,I,i,z))
     # sex in ptrs -------------------------------------------------------------
     # TODO
   }
   # clean-up ------------------------------------------------------------------
   if (rm.dum){ # remove initial dummy population
-    i = which(Is$age < (P$zf/z1y+amin))
-    Is = Is[i,]
-    Es = lapply(Es,`[`,i)
+    i = which(I$z.born > -amin*z1y)
+    I = I[i,]
+    E = lapply(E,`[`,i)
   }
-  S = list(P=P,Is=Is,Es=Es)
+  M = list(P=P,I=I,E=E)
 }
 
 sim.runs = function(Ps,.par=TRUE){
   # run.sim in parallel for each (P)arameter set in Ps
-  Ss = par.lapply(Ps,sim.run,.par=.par & len(Ps)>1)
+  Ms = par.lapply(Ps,sim.run,.par=.par)
 }
