66b87d8d429594ad977efe0ec7067724260800d6
jessexknight <jesse.x.knight@protonmail.com>
2024-08-08 11:06:02 +0100

    remove code.py

--- code.py/profile/run.sh
+++
@@ -1,8 +0,0 @@
-# by code line (inaccurate)
-for py in model/*.py; do sed -i 's/\#\@profile/\@profile/' $py; done
-(date; kernprof -lv -u 1 profile/run.py) > profile/profile.l.txt
-for py in model/*.py; do sed -i 's/\@profile/\#\@profile/' $py; done
-rm run.py.lprof
-# by code element (accurate)
-(date; kernprof -v -u 1 profile/run.py) > profile/profile.e.txt
-rm run.py.prof
