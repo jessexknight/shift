a3ecafef0926b0a3c8c271f2dffe4b46ea58736c
jessexknight <jesse.x.knight@protonmail.com>
2024-08-12 12:32:17 +0100

    clean-up val: fix zf/nt bug, add p3m for tRR

--- code/sim/srv.r
+++ code/sim/srv.r
@@ -68,10 +68,24 @@ srv.val = function(P,Q,E,z){
   Q$ptr_o.n1y = sapply(E$ptr_o,num.dz,z,z1y)
   Q$ptr_x.n1y = sapply(E$ptr_x,num.dz,z,z1y)
   # states 1 year prior
-  Q = cbind(Q,srv.base(P,Q,E,z-z1y,fmt='yp.%s'))
-  Q$yp.dep.dur.c = int.cut(Q$yp.dep.dur,z1y*c(0,1,10))
-  Q$yp.haz.dur.c = int.cut(Q$yp.haz.dur,z1y*c(0,1,10))
-  Q$yp.vio.nt.c  = int.cut(Q$yp.vio.nt,c(0,1,10,100))
+  Q = cbind(Q,srv.base(P,Q,E,z-z1y,fmt='p1y.%s'))
+  Q$p1y.dep.dur.c = int.cut(Q$p1y.dep.dur,z1y*c(0,1,10))
+  Q$p1y.haz.dur.c = int.cut(Q$p1y.haz.dur,z1y*c(0,1,10))
+  Q$p1y.vio.nt.c  = int.cut(Q$p1y.vio.nt,c(0,1,10,100))
+  # events in past 3 months
+  Q$vio.n3m   = sapply(E$vio,  num.dz,z,z3m)
+  Q$vio.a3m   = sapply(E$vio,  any.dz,z,z3m)
+  Q$dep_o.a3m = sapply(E$dep_o,any.dz,z,z3m)
+  Q$dep_x.a3m = sapply(E$dep_x,any.dz,z,z3m)
+  Q$haz_o.a3m = sapply(E$haz_o,any.dz,z,z3m)
+  Q$haz_x.a3m = sapply(E$haz_x,any.dz,z,z3m)
+  Q$ptr_o.n3m = sapply(E$ptr_o,num.dz,z,z3m)
+  Q$ptr_x.n3m = sapply(E$ptr_x,num.dz,z,z3m)
+  # states 3 months prior
+  Q = cbind(Q,srv.base(P,Q,E,z-z3m,fmt='p3m.%s'))
+  Q$p3m.dep.dur.c = int.cut(Q$p3m.dep.dur,z3m*c(0,1,10))
+  Q$p3m.haz.dur.c = int.cut(Q$p3m.haz.dur,z3m*c(0,1,10))
+  Q$p3m.vio.nt.c  = int.cut(Q$p3m.vio.nt,c(0,1,10,100))
   return(Q)
 }
 
--- code/sim/val.r
+++ code/sim/val.r
@@ -11,36 +11,36 @@ key.vars = c('age',
 
 vals = list(
   # base rates
-  'Ri.all'=list(save=NULL,vars=key.vars,among=quote(sex.act)),
+  'Ri.all'=list(save=NULL,vars=key.vars,among=quote(p1y.sex.act)),
   # RR age
   'aRR.vio'=list(save=c('aRR.vio'),  vars=c('vio.n1y'),  strat='age.10'),
   'aRR.dep'=list(save=c('aRR.dep_o'),vars=c('dep_o.a1y'),strat='age.10'),
   'aRR.haz'=list(save=c('aRR.haz_o'),vars=c('haz_o.a1y'),strat='age.10'),
-  'aRR.ptr'=list(save=c('aRR.ptr_o'),vars=c('ptr_o.n1y'),strat='age.10',among=quote(sex.act)),
+  'aRR.ptr'=list(save=c('aRR.ptr_o'),vars=c('ptr_o.n1y'),strat='age.10',among=quote(p1y.sex.act)),
   # basic RR
-  'RR.dep_o.dep_p'=list(save='RR.dep_o.dep_p',vars='dep_o.a1y',strat='yp.dep.past',among=quote(!yp.dep.now)),
-  'RR.haz_o.haz_p'=list(save='RR.haz_o.haz_p',vars='haz_o.a1y',strat='yp.haz.past',among=quote(!yp.haz.now)),
-  'RR.haz_o.dep_w'=list(save='RR.haz_o.dep_w',vars='haz_o.a1y',strat='yp.dep.now', among=quote(!yp.haz.now)),
-  'RR.haz_x.dep_w'=list(save='RR.haz_x.dep_w',vars='haz_x.a1y',strat='yp.dep.now', among=quote( yp.haz.now)),
-  'RR.ptr_o.dep_w'=list(save='RR.ptr_o.dep_w',vars='ptr_o.n1y',strat='yp.dep.now', among=quote(sex.act)),
-  'RR.ptr_o.haz_w'=list(save='RR.ptr_o.haz_w',vars='ptr_o.n1y',strat='yp.haz.now', among=quote(sex.act)),
-  'RR.ptr_x.dep_w'=list(save='RR.ptr_x.dep_w',vars='ptr_x.n1y',strat='yp.dep.now', among=quote(sex.act)),
-  'RR.ptr_x.haz_w'=list(save='RR.ptr_x.haz_w',vars='ptr_x.n1y',strat='yp.haz.now', among=quote(sex.act)),
+  'RR.dep_o.dep_p'=list(save='RR.dep_o.dep_p',vars='dep_o.a1y',strat='p1y.dep.past',among=quote(!p1y.dep.now)),
+  'RR.haz_o.haz_p'=list(save='RR.haz_o.haz_p',vars='haz_o.a1y',strat='p1y.haz.past',among=quote(!p1y.haz.now)),
+  'RR.haz_o.dep_w'=list(save='RR.haz_o.dep_w',vars='haz_o.a1y',strat='p1y.dep.now', among=quote(!p1y.haz.now)),
+  'RR.haz_x.dep_w'=list(save='RR.haz_x.dep_w',vars='haz_x.a1y',strat='p1y.dep.now', among=quote( p1y.haz.now)),
+  'RR.ptr_o.dep_w'=list(save='RR.ptr_o.dep_w',vars='ptr_o.n1y',strat='p1y.dep.now', among=quote( p1y.sex.act)),
+  'RR.ptr_o.haz_w'=list(save='RR.ptr_o.haz_w',vars='ptr_o.n1y',strat='p1y.haz.now', among=quote( p1y.sex.act)),
+  'RR.ptr_x.dep_w'=list(save='RR.ptr_x.dep_w',vars='ptr_x.n1y',strat='p1y.dep.now', among=quote( p1y.sex.act)),
+  'RR.ptr_x.haz_w'=list(save='RR.ptr_x.haz_w',vars='ptr_x.n1y',strat='p1y.haz.now', among=quote( p1y.sex.act)),
   # transient RR
-  'tRR.dep_o.vio_z'=list(save=c('iRR.dep_o.vio_z','tsc.dep_o.vio_z'),vars='dep_o.a1y',strat='vio.a1y',among=quote(!yp.dep.now)),
-  'tRR.dep_x.vio_z'=list(save=c('iRR.dep_x.vio_z','tsc.dep_x.vio_z'),vars='dep_x.a1y',strat='vio.a1y',among=quote( yp.dep.now)),
-  'tRR.haz_o.vio_z'=list(save=c('iRR.haz_o.vio_z','tsc.haz_o.vio_z'),vars='haz_o.a1y',strat='vio.a1y',among=quote(!yp.haz.now)),
-  'tRR.haz_x.vio_z'=list(save=c('iRR.haz_x.vio_z','tsc.haz_x.vio_z'),vars='haz_x.a1y',strat='vio.a1y',among=quote( yp.haz.now)),
-  'tRR.ptr_o.vio_z'=list(save=c('iRR.ptr_o.vio_z','tsc.ptr_o.vio_z'),vars='ptr_o.n1y',strat='vio.a1y',among=quote(sex.act)),
+  'tRR.dep_o.vio_zf'=list(save=c('iRR.dep_o.vio_zf','tsc.dep_o.vio_zf'),vars='dep_o.a3m',strat='vio.a3m',among=quote(!p3m.dep.now)),
+  'tRR.dep_x.vio_zf'=list(save=c('iRR.dep_x.vio_zf','tsc.dep_x.vio_zf'),vars='dep_x.a3m',strat='vio.a3m',among=quote( p3m.dep.now)),
+  'tRR.haz_o.vio_zf'=list(save=c('iRR.haz_o.vio_zf','tsc.haz_o.vio_zf'),vars='haz_o.a3m',strat='vio.a3m',among=quote(!p3m.haz.now)),
+  'tRR.haz_x.vio_zf'=list(save=c('iRR.haz_x.vio_zf','tsc.haz_x.vio_zf'),vars='haz_x.a3m',strat='vio.a3m',among=quote( p3m.haz.now)),
+  'tRR.ptr_o.vio_zf'=list(save=c('iRR.ptr_o.vio_zf','tsc.ptr_o.vio_zf'),vars='ptr_o.n3m',strat='vio.a3m',among=quote( p3m.sex.act)),
   # cumulative RR
-  'nRR.dep_o.vio_n'=list(save=c('mRR.dep_o.vio_n','nsc.dep_o.vio_n'),vars='dep_o.a1y',strat='yp.vio.nt.c',among=quote(!yp.dep.now)),
-  'nRR.haz_o.vio_n'=list(save=c('mRR.haz_o.vio_n','nsc.haz_o.vio_n'),vars='haz_o.a1y',strat='yp.vio.nt.c',among=quote(!yp.haz.now)),
-  'nRR.ptr_o.vio_n'=list(save=c('mRR.ptr_o.vio_n','nsc.ptr_o.vio_n'),vars='ptr_o.n1y',strat='yp.vio.nt.c',among=quote(sex.act)),
+  'nRR.dep_o.vio_nt'=list(save=c('mRR.dep_o.vio_nt','nsc.dep_o.vio_nt'),vars='dep_o.a1y',strat='p1y.vio.nt.c',among=quote(!p1y.dep.now)),
+  'nRR.haz_o.vio_nt'=list(save=c('mRR.haz_o.vio_nt','nsc.haz_o.vio_nt'),vars='haz_o.a1y',strat='p1y.vio.nt.c',among=quote(!p1y.haz.now)),
+  'nRR.ptr_o.vio_nt'=list(save=c('mRR.ptr_o.vio_nt','nsc.ptr_o.vio_nt'),vars='ptr_o.n1y',strat='p1y.vio.nt.c',among=quote( p1y.sex.act)),
   # duration RR
-  'dRR.dep_x.dep_u'=list(save=c('dsc.dep_x.dep_u'),vars='dep_x.a1y',strat='yp.dep.dur.c',among=quote(yp.dep.now)),
-  'dRR.haz_x.haz_u'=list(save=c('dsc.haz_x.haz_u'),vars='haz_x.a1y',strat='yp.haz.dur.c',among=quote(yp.dep.now)),
+  'dRR.dep_x.dep_u'=list(save=c('dsc.dep_x.dep_u'),vars='dep_x.a1y',strat='p1y.dep.dur.c',among=quote(p1y.dep.now)),
+  'dRR.haz_x.haz_u'=list(save=c('dsc.haz_x.haz_u'),vars='haz_x.a1y',strat='p1y.haz.dur.c',among=quote(p1y.dep.now)),
   # default
-  'default'=ulist(lapply(null.all,function(re){ NULL }),vars=key.vars,among=quote(sex.act))
+  'default'=ulist(lapply(null.all,function(re){ NULL }),vars=key.vars,among=quote(p1y.sex.act))
 )
 for (v in names(vals)){ vals[[v]]$name = v }
 
@@ -48,7 +48,7 @@ for (v in names(vals)){ vals[[v]]$name = v }
 # run & plot
 
 val.run = function(name,vars,among=quote(TRUE),strat='.',...){
-  Ps = lapply(1:7,get.pars,n=333,
+  Ps = lapply(1:21,get.pars,n=333,
     null=ulist('Ri\\.m$'=NULL,...))
   Q = srv.apply(sim.runs(Ps),srvs=c(srv.val))
   Q = subset(Q,age < amax & eval(among))
@@ -92,4 +92,4 @@ val.plot = function(Q,vars,strat='.'){
 # main
 
 for (val in vals){
-  do.call(val.run,val,quote=TRUE); break }
+  do.call(val.run,val,quote=TRUE) }
