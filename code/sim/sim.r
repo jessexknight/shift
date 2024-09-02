5d8a33b0df916de2beb4aee1d5350389d33e05de
jessexknight <jesse.x.knight@protonmail.com>
2024-09-02 14:54:25 +0100

    code: sim outputs in 't' not 'z' & minor rename

--- code/sim/sim.r
+++ code/sim/sim.r
@@ -5,7 +5,7 @@
 init.evts = function(P){
   # initialize event matrices for each event type & individual
   # TODO: could reduce mem req using ncol = adur*P$z1y < P$zf
-  E = lapply(evts,function(e){ matrix(FALSE,nrow=P$n.tot,ncol=P$zf) })
+  Z = lapply(evts,function(e){ matrix(FALSE,nrow=P$n.tot,ncol=P$zf) })
 }
 
 init.inds = function(P){
@@ -37,12 +37,12 @@ init.inds = function(P){
   cdm.Pi = rbeta(n=n,shape1=P$cdm.Pi.shapes[1],shape2=P$cdm.Pi.shapes[2])
   I = data.frame(
     i = seq(n),
-    z.born  = -age*P$z1y,
+    t.born  = -age*P$t1y,
     age     = +age,
     age.act = runif(n,min=amin,max=20),
     # violence
     vio.Ri = vio.Ri,
-    vio.zf = NA,
+    vio.zr = NA,
     vio.nt = 0,
     # depression
     dep_o.Ri = dep$o.Ri,
@@ -103,7 +103,7 @@ rate.dep_o = function(P,J,R,aj,z){
       J$dep_o.Ri[j] # base rate
     * P$aRR.dep_o[aj[j]] # RR age
     * (1 + P$RRu.dep_o.dep_p * J$dep.past[j]) # RR dep past
-    * map.tRR(P$tRRu.dep_o.vio_zr,J$vio.zf[j],z) # tRR vio
+    * map.tRR(P$tRRu.dep_o.vio_zr,J$vio.zr[j],z) # tRR vio
     * P$nRR.dep_o.vio_nt[J$vio.nt[j]+1] # nRR vio
 ); return(R) }
 
@@ -112,7 +112,7 @@ rate.dep_x = function(P,J,R,aj,z){
   R[j] = ( # among dep
       J$dep_x.Ri[j] # base rate
     * map.tRR(P$dRRu.dep_x.dep_u,J$dep.zo[j],z) # RR dep dur
-    * map.tRR(P$tRRu.dep_x.vio_zr,J$vio.zf[j],z) # tRR vio
+    * map.tRR(P$tRRu.dep_x.vio_zr,J$vio.zr[j],z) # tRR vio
 ); return(R) }
 
 rate.haz_o = function(P,J,R,aj,z){
@@ -122,7 +122,7 @@ rate.haz_o = function(P,J,R,aj,z){
     * P$aRR.haz_o[aj[j]] # RR age
     * (1 + P$RRu.haz_o.haz_p * J$haz.past[j]) # RR haz past
     * (1 + P$RRu.haz_o.dep_w * J$dep.now[j]) # RR dep now
-    * map.tRR(P$tRRu.haz_o.vio_zr,J$vio.zf[j],z) # tRR vio
+    * map.tRR(P$tRRu.haz_o.vio_zr,J$vio.zr[j],z) # tRR vio
     * P$nRR.haz_o.vio_nt[J$vio.nt[j]+1] # nRR vio
 ); return(R) }
 
@@ -132,7 +132,7 @@ rate.haz_x = function(P,J,R,aj,z){
       J$haz_x.Ri[j] # base rate
     * map.tRR(P$dRRu.haz_x.haz_u,J$haz.zo[j],z) # RR haz dur
     * (1 + P$RRu.haz_x.dep_w * J$dep.now[j]) # RR dep now
-    * map.tRR(P$tRRu.haz_x.vio_zr,J$vio.zf[j],z) # tRR vio
+    * map.tRR(P$tRRu.haz_x.vio_zr,J$vio.zr[j],z) # tRR vio
 ); return(R) }
 
 rate.ptr_o = function(P,J,R,aj,z){
@@ -142,7 +142,7 @@ rate.ptr_o = function(P,J,R,aj,z){
     * P$aRR.ptr_o[aj[j]] # RR age
     * (1 + P$RRu.ptr_o.dep_w * J$dep.now[j]) # RR dep now
     * (1 + P$RRu.ptr_o.haz_w * J$haz.now[j]) # RR haz now
-    * map.tRR(P$tRRu.ptr_o.vio_zr,J$vio.zf[j],z) # tRR vio
+    * map.tRR(P$tRRu.ptr_o.vio_zr,J$vio.zr[j],z) # tRR vio
     * P$nRR.ptr_o.vio_nt[J$vio.nt[j]+1] # nRR vio
 ); return(R) }
 
@@ -164,7 +164,7 @@ sim.run = function(P,sub='act'){
   # initialization ------------------------------------------------------------
   set.seed(P$seed)
   I = init.inds(P) # individuals
-  E = init.evts(P) # events
+  Z = init.evts(P) # events
   K = NULL         # partnerships
   # event loop ----------------------------------------------------------------
   for (z in 1:P$zf){
@@ -175,7 +175,7 @@ sim.run = function(P,sub='act'){
     b = rate.to.bool(rate.ptr_x(P,K,I),P$dtz)
     ni = tabulate(as.numeric(K[b,1:2]),P$n.tot)
     I$ptr.nw = I$ptr.nw - ni
-    E$ptr_x[,z] = ni
+    Z$ptr_x[,z] = ni
     K = K[!b,]
     # select active inds ------------------------------------------------------
     i = which(I$age > amin & I$age < amax)
@@ -185,40 +185,42 @@ sim.run = function(P,sub='act'){
     R0 = numeric(nrow(J)) # init rate = 0 for j
     # vio events --------------------------------------------------------------
     i = ij[which(rate.to.bool(rate.vio(P,J,aj),P$dtz))]
-    I$vio.zf[i] = z
+    I$vio.zr[i] = z
     I$vio.nt[i] = I$vio.nt[i] + 1
-    E$vio[i,z]  = TRUE
+    Z$vio[i,z]  = TRUE
     # begin dep ---------------------------------------------------------------
     i = ij[which(rate.to.bool(rate.dep_o(P,J,R0,aj,z),P$dtz))]
     I$dep.now[i] = TRUE
     I$dep.past[i] = TRUE
     I$dep.zo[i] = z
-    E$dep_o[i,z] = TRUE
+    Z$dep_o[i,z] = TRUE
     # end dep -----------------------------------------------------------------
     i = ij[which(rate.to.bool(rate.dep_x(P,J,R0,aj,z),P$dtz))]
     I$dep.now[i] = FALSE
-    E$dep_x[i,z] = TRUE
+    Z$dep_x[i,z] = TRUE
     # begin haz ---------------------------------------------------------------
     i = ij[which(rate.to.bool(rate.haz_o(P,J,R0,aj,z),P$dtz))]
     I$haz.now[i] = TRUE
     I$haz.past[i] = TRUE
     I$haz.zo[i] = z
-    E$haz_o[i,z] = TRUE
+    Z$haz_o[i,z] = TRUE
     # end haz -----------------------------------------------------------------
     i = ij[which(rate.to.bool(rate.haz_x(P,J,R0,aj,z),P$dtz))]
     I$haz.now[i] = FALSE
-    E$haz_x[i,z] = TRUE
+    Z$haz_x[i,z] = TRUE
     # begin ptrs --------------------------------------------------------------
     nj = even.sum(rate.to.num(rate.ptr_o(P,J,R0,aj,z),P$dtz))
     I$ptr.nw[J$i] = I$ptr.nw[J$i] + nj
-    E$ptr_o[J$i,z] = nj
+    Z$ptr_o[J$i,z] = nj
     K = rbind(K,init.ptrs(P,I,rep(J$i,nj),z))
     # sex in ptrs -------------------------------------------------------------
     # TODO
   }
   # clean-up ------------------------------------------------------------------
-  E = lapply(E,apply,1,function(ni){ rep(1:P$zf,ni) }) # n mat -> z vecs
-  M = sim.sub(M=list(P=P,I=I,E=E),sub=sub)
+  x = c('vio.zr','dep.zo','haz.zo')
+  I[gsub('z','t',x)] = P$dtz * I[x]; I[x] = NULL # convert *.z -> *.t
+  E = lapply(Z,apply,1,function(ni){ P$dtz*rep(1:P$zf,ni) }) # n mat -> t vecs
+  M = sim.sub(M=list(P=P,I=I,E=E),sub=sub) # collect
 }
 
 sim.runs = function(Ps,.par=TRUE){
@@ -232,7 +234,7 @@ sim.sub = function(M,sub){
   # subset model output by age
   i = switch(sub,
     act = which(M$I$age > amin & M$I$age < amax),
-    dum = which(M$I$z.born > -amin*M$P$z1y),
+    dum = which(M$I$t.born > -amin*M$P$t1y),
     all = 1:nrow(M$I))
   M = list(P=M$P,I=M$I[i,],E=lapply(M$E,`[`,i))
 }
