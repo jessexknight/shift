e4f711ad47eab41926af314a2023c8ec048e20c3
jessexknight <jesse.x.knight@protonmail.com>
2024-09-02 16:28:45 +0100

    code: update srv for E$ z -> t (except inc)

--- code/sim/srv.r
+++ code/sim/srv.r
@@ -15,7 +15,7 @@
 # survey funs
 
 srv.apply = function(Ms,t,srvs=c(srv.base),p.vars=NULL,i.vars=NULL){
-  # apply 1+ surveys (srvs) to sim outputs (Ms) at time (z)
+  # apply 1+ surveys (srvs) to sim outputs (Ms) at time (t)
   status(3,'srv.apply: ',len(Ms))
   if (missing(t)){ t = Ms[[1]]$P$tf }
   Ps = lapply(Ms,`[[`,'P')
@@ -35,70 +35,58 @@ srv.init = function(M,t,p.vars,i.vars){
 }
 
 srv.base = function(P,Q,E,t,fmt='%s'){
-  z = t/P$dtz
-  E = lapply(E,lapply,clip.zes,z=z) # clip events
-  Q$age      = (z-Q$z.born)/P$z1y
+  E = clip.evts(E,t=t)
+  Q$age      = (t-Q$t.born)/P$t1y
   Q$sex.act  = Q$age > Q$age.act
   Q$vio.nt   = sapply(E$vio,len)
-  Q$vio.zf   = sapply(E$vio,last)
+  Q$vio.tr   = sapply(E$vio,last)
   Q$dep.now  = sapply(E$dep_o,len) > sapply(E$dep_x,len)
   Q$dep.past = sapply(E$dep_o,len) > 0
-  Q$dep.zo   = sapply(E$dep_o,last)
-  Q$dep.uz   = ifelse(Q$dep.now,z+1-Q$dep.zo,NA)
+  Q$dep.to   = sapply(E$dep_o,last)
+  Q$dep.dur  = ifelse(Q$dep.now,t-Q$dep.to,NA)
   Q$haz.now  = sapply(E$haz_o,len) > sapply(E$haz_x,len)
   Q$haz.past = sapply(E$haz_o,len) > 0
   Q$haz.zo   = sapply(E$haz_o,last)
-  Q$haz.uz   = ifelse(Q$haz.now,z+1-Q$haz.zo,NA)
+  Q$haz.dur  = ifelse(Q$haz.now,t-Q$haz.to,NA)
   Q$ptr.nt   = sapply(E$ptr_o,len)
   Q$ptr.nw   = Q$ptr.nt - sapply(E$ptr_x,len)
   names(Q) = sprintf(fmt,names(Q)) # format names for conflicts
   return(Q)
 }
 
-srv.ptr = function(P,Q,E,t){
-  z = t/P$dtz
-  E = lapply(E,lapply,clip.zes,z=z) # clip events
-  Q$ptr.nw.c = int.cut(Q$ptr.nw,c(0,1,2))
-  ptr.w.uz = wapply(dur.ptr.w,E$ptr_o,E$ptr_x,E$ptr_u,z) # ongo ptrs durs
-  Q$ptr.p.dur.m = P$dtz*sapply(E$ptr_u,mean)  # past ptrs durs
-  Q$ptr.w.dur.m = P$dtz*sapply(ptr.w.uz,mean) # ongo ptrs durs
-  return(Q)
-}
-
 # -----------------------------------------------------------------------------
 
 srv.val.RR = function(P,Q,E,t){
-  z = t/P$dtz
-  E = lapply(E,lapply,clip.zes,z=z) # clip events
+  E = clip.evts(E,t=t)
   Q = srv.base(P,Q,E,t)
   Q$age.10 = floor(Q$age/10)*10
   # events in past 1 year
-  Q$vio.n1y   = sapply(E$vio,  num.dz,z,P$z1y)
-  Q$vio.a1y   = sapply(E$vio,  any.dz,z,P$z1y)
-  Q$dep_o.a1y = sapply(E$dep_o,any.dz,z,P$z1y)
-  Q$dep_x.a1y = sapply(E$dep_x,any.dz,z,P$z1y)
-  Q$haz_o.a1y = sapply(E$haz_o,any.dz,z,P$z1y)
-  Q$haz_x.a1y = sapply(E$haz_x,any.dz,z,P$z1y)
-  Q$ptr_o.n1y = sapply(E$ptr_o,num.dz,z,P$z1y)
-  Q$ptr_x.n1y = sapply(E$ptr_x,num.dz,z,P$z1y)
+  Q$vio.n1y   = sapply(E$vio,  num.dt,t,P$t1y)
+  Q$vio.a1y   = sapply(E$vio,  any.dt,t,P$t1y)
+  Q$dep_o.a1y = sapply(E$dep_o,any.dt,t,P$t1y)
+  Q$dep_x.a1y = sapply(E$dep_x,any.dt,t,P$t1y)
+  Q$haz_o.a1y = sapply(E$haz_o,any.dt,t,P$t1y)
+  Q$haz_x.a1y = sapply(E$haz_x,any.dt,t,P$t1y)
+  Q$ptr_o.n1y = sapply(E$ptr_o,num.dt,t,P$t1y)
+  Q$ptr_x.n1y = sapply(E$ptr_x,num.dt,t,P$t1y)
   # states 1 year prior
   Q = cbind(Q,srv.base(P,Q,E,t-P$t1y,fmt='p1y.%s'))
-  Q$p1y.dep.dur.c = int.cut(Q$p1y.dep.uz/P$z1y,c(0,1,5))
-  Q$p1y.haz.dur.c = int.cut(Q$p1y.haz.uz/P$z1y,c(0,1,5))
+  Q$p1y.dep.dur.c = int.cut(Q$p1y.dep.dur/P$t1y,c(0,1,5))
+  Q$p1y.haz.dur.c = int.cut(Q$p1y.haz.dur/P$t1y,c(0,1,5))
   Q$p1y.vio.nt.c  = int.cut(Q$p1y.vio.nt,c(0,3,30))
   # events in past 3 months
-  Q$vio.n3m   = sapply(E$vio,  num.dz,z,P$z3m)
-  Q$vio.a3m   = sapply(E$vio,  any.dz,z,P$z3m)
-  Q$dep_o.a3m = sapply(E$dep_o,any.dz,z,P$z3m)
-  Q$dep_x.a3m = sapply(E$dep_x,any.dz,z,P$z3m)
-  Q$haz_o.a3m = sapply(E$haz_o,any.dz,z,P$z3m)
-  Q$haz_x.a3m = sapply(E$haz_x,any.dz,z,P$z3m)
-  Q$ptr_o.n3m = sapply(E$ptr_o,num.dz,z,P$z3m)
-  Q$ptr_x.n3m = sapply(E$ptr_x,num.dz,z,P$z3m)
+  Q$vio.n3m   = sapply(E$vio,  num.dt,t,P$t3m)
+  Q$vio.a3m   = sapply(E$vio,  any.dt,t,P$t3m)
+  Q$dep_o.a3m = sapply(E$dep_o,any.dt,t,P$t3m)
+  Q$dep_x.a3m = sapply(E$dep_x,any.dt,t,P$t3m)
+  Q$haz_o.a3m = sapply(E$haz_o,any.dt,t,P$t3m)
+  Q$haz_x.a3m = sapply(E$haz_x,any.dt,t,P$t3m)
+  Q$ptr_o.n3m = sapply(E$ptr_o,num.dt,t,P$t3m)
+  Q$ptr_x.n3m = sapply(E$ptr_x,num.dt,t,P$t3m)
   # states 3 months prior
   Q = cbind(Q,srv.base(P,Q,E,t-P$t3m,fmt='p3m.%s'))
-  Q$p3m.dep.dur.c = int.cut(Q$p3m.dep.uz/P$z1y,c(0,1,5))
-  Q$p3m.haz.dur.c = int.cut(Q$p3m.haz.uz/P$z1y,c(0,1,5))
+  Q$p3m.dep.dur.c = int.cut(Q$p3m.dep.dur/P$t1y,c(0,1,5))
+  Q$p3m.haz.dur.c = int.cut(Q$p3m.haz.dur/P$t1y,c(0,1,5))
   Q$p3m.vio.nt.c  = int.cut(Q$p3m.vio.nt,c(0,3,30))
   return(Q)
 }
@@ -150,10 +138,10 @@ inc.rate = function(Y,e,strat=NULL){
 # =============================================================================
 # question / event funs
 
-clip.zes = function(zes,z){ zes[zes <= z] }
+clip.evts = function(E,t){ E = lapply(E,lapply,clip.tes,t=t) }
 
-num.dz = function(zes,z,dz){ n = sum(zes <= z & zes >= z+1-dz) }
+clip.tes = function(tes,t){ tes[tes <= t] }
 
-any.dz = function(zes,z,dz){ b = num.dz(zes,z,dz) > 0 }
+num.dt = function(tes,t,dt){ n = sum(tes <= t & tes > t-dt) }
 
-dur.ptr.w = function(zos,zxs,us,z){ z - zos[!(zos %in% (zxs-us))] }
+any.dt = function(tes,t,dt){ b = num.dt(tes,t,dt) > 0 }
