d580cd72580135a226d5c21035473b5c51b747ce
jessexknight <jesse.x.knight@protonmail.com>
2024-09-10 12:40:16 -0400

    code: update hpc

--- code/hpc/mwe.sh
+++ code/hpc/mwe.sh
@@ -2,7 +2,7 @@
 #PBS -j oe
 #PBS -N mwe
 #PBS -l walltime=00:01:00
-#PBS -l select=1:ncpus=1:mem=1gb
+#PBS -l select=1:ncpus=1:mem=4gb
 
 module add tools/prod
 module add R/4.1.2-foss-2021b
--- code/hpc/val.sh
+++ code/hpc/val.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -j oe
 #PBS -N val
-#PBS -l walltime=00:45:00
-#PBS -l select=1:ncpus=64:mem=128gb
+#PBS -l walltime=01:00:00
+#PBS -l select=1:ncpus=64:mem=256gb
 
 module add tools/prod
 module add R/4.1.2-foss-2021b
@@ -10,5 +10,5 @@ module add R/4.1.2-foss-2021b
 cd $PBS_O_WORKDIR
 
 for v in {1..23}; do
-  Rscript sim/val.r v=$v n=1000 n.seed=128 .cores=64 .mem=128
+  Rscript sim/val.r v=$v n=1000 n.seed=128 .cores=64
 done
