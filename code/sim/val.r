3140599e2f2e046af15130d6de8431171189a990
jessexknight <jesse.x.knight@protonmail.com>
2024-09-24 09:46:43 -0400

    code: merge vals

--- code/sim/val.r
+++ code/sim/val.r
@@ -22,12 +22,6 @@ P0 = list(
 # =============================================================================
 # scenarios
 
-val.Ri = list(
-  Ri.m=list(pars=ulist(P0),evts=evts[1:7])
-)
-
-# -----------------------------------------------------------------------------
-
 RR1 = c(3)
 RR2 = c(1,3)
 RR3 = c(1,2,3)
@@ -40,13 +34,15 @@ aRR5 = c( 1, 5, 2, 4, 3)/3
 age.10.fun   = function(Q){ floor(Q$age.1/10)*10 }
 vio.nt.c.fun = function(Q){ int.cut(Q$vio.nt,c(0,ns2)) }
 
-val.RR = list(
-  # RR age [1:4]
+vals = list(
+  # base rates [1]
+  Ri.m=list(pars=ulist(P0),evts=evts[1:7]),
+  # RR age [2:5]
   aRR.vio=list(pars=ulist(P0,aRR.vio.RRs  =aRR5,aRR.vio.ages  =age5,aRR.shape='const'),evts='vio',  strat='age.10',x.cols=list(age.10=age.10.fun)),
   aRR.dep=list(pars=ulist(P0,aRR.dep_o.RRs=aRR5,aRR.dep_o.ages=age5,aRR.shape='const'),evts='dep_o',strat='age.10',x.cols=list(age.10=age.10.fun)),
   aRR.haz=list(pars=ulist(P0,aRR.haz_o.RRs=aRR5,aRR.haz_o.ages=age5,aRR.shape='const'),evts='haz_o',strat='age.10',x.cols=list(age.10=age.10.fun)),
   aRR.ptr=list(pars=ulist(P0,aRR.ptr_o.RRs=aRR5,aRR.ptr_o.ages=age5,aRR.shape='const'),evts='ptr_o',strat='age.10',x.cols=list(age.10=age.10.fun)),
-  # basic RR [5:12]
+  # basic RR [6:13]
   RR.dep_o.dep_p=list(pars=ulist(P0,RR.dep_o.dep_p=  RR3),evts='dep_o',strat='dep.past'),
   RR.haz_o.haz_p=list(pars=ulist(P0,RR.haz_o.haz_p=  RR3),evts='haz_o',strat='haz.past'),
   RR.haz_o.dep_w=list(pars=ulist(P0,RR.haz_o.dep_w=  RR3),evts='haz_o',strat='dep.now'),
@@ -55,17 +51,17 @@ val.RR = list(
   RR.ptr_o.haz_w=list(pars=ulist(P0,RR.ptr_o.haz_w=  RR3),evts='ptr_o',strat='haz.now'),
   RR.ptr_x.dep_w=list(pars=ulist(P0,RR.ptr_x.dep_w=  RR3),evts='ptr_x',strat='dep.now'),
   RR.ptr_x.haz_w=list(pars=ulist(P0,RR.ptr_x.haz_w=  RR3),evts='ptr_x',strat='haz.now'),
-  # transient RR [13:17]
+  # transient RR [14:18]
   tRR.dep_o.vio_zr=list(pars=ulist(P0,iRR.dep_o.vio_zr=  RR2,tsc.dep_o.vio_zr=ts2,tRR.shape='step'),evts='dep_o',strat='vio.dt.c',e.dts=list(vio=ts2)),
   tRR.dep_x.vio_zr=list(pars=ulist(P0,iRR.dep_x.vio_zr=1/RR2,tsc.dep_x.vio_zr=ts2,tRR.shape='step'),evts='dep_x',strat='vio.dt.c',e.dts=list(vio=ts2)),
   tRR.haz_o.vio_zr=list(pars=ulist(P0,iRR.haz_o.vio_zr=  RR2,tsc.haz_o.vio_zr=ts2,tRR.shape='step'),evts='haz_o',strat='vio.dt.c',e.dts=list(vio=ts2)),
   tRR.haz_x.vio_zr=list(pars=ulist(P0,iRR.haz_x.vio_zr=1/RR2,tsc.haz_x.vio_zr=ts2,tRR.shape='step'),evts='haz_x',strat='vio.dt.c',e.dts=list(vio=ts2)),
   tRR.ptr_o.vio_zr=list(pars=ulist(P0,iRR.ptr_o.vio_zr=  RR2,tsc.ptr_o.vio_zr=ts2,tRR.shape='step'),evts='ptr_o',strat='vio.dt.c',e.dts=list(vio=ts2)),
-  # cumulative RR [18:20]
+  # cumulative RR [19:21]
   nRR.dep_o.vio_nt=list(pars=ulist(P0,mRR.dep_o.vio_nt=RR2,nsc.dep_o.vio_nt=ns2,nRR.shape='step'),evts='dep_o',strat='vio.nt.c',x.cols=list(vio.nt.c=vio.nt.c.fun)),
   nRR.haz_o.vio_nt=list(pars=ulist(P0,mRR.haz_o.vio_nt=RR2,nsc.haz_o.vio_nt=ns2,nRR.shape='step'),evts='haz_o',strat='vio.nt.c',x.cols=list(vio.nt.c=vio.nt.c.fun)),
   nRR.ptr_o.vio_nt=list(pars=ulist(P0,mRR.ptr_o.vio_nt=RR2,nsc.ptr_o.vio_nt=ns2,nRR.shape='step'),evts='ptr_o',strat='vio.nt.c',x.cols=list(vio.nt.c=vio.nt.c.fun)),
-  # duration RR [21:22]
+  # duration RR [22:23]
   dRR.dep_x.dep_u=list(pars=ulist(P0,dsc.dep_x.dep_u=ds2,dRR.shape='step'),evts='dep_x',strat='dep_o.dt.c',e.dts=list(dep_o=ds2)),
   dRR.haz_x.haz_u=list(pars=ulist(P0,dsc.haz_x.haz_u=ds2,dRR.shape='step'),evts='haz_x',strat='haz_o.dt.c',e.dts=list(haz_o=ds2))
 )
@@ -147,6 +143,5 @@ rmed = function(x){ round(median(x)) }
 # =============================================================================
 # main
 
-vals = c(val.Ri,val.RR)[v]
 for (name in names(vals)){ vals[[name]]$name = name }
-for (val in vals){ do.call(val.run,val,quote=TRUE) }
+for (val in vals[v]){ do.call(val.run,val,quote=TRUE) }
