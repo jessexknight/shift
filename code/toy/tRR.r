b4507d0cb4c17c9ea9750e47c863ca4f2e1903b7
jessexknight <jesse.x.knight@protonmail.com>
2024-10-18 10:34:08 +0100

    code: clean-up toy tRR + add dRR

--- code/toy/tRR.r
+++ code/toy/tRR.r
@@ -12,42 +12,59 @@ tx = seq(0,tmax,dti)    # for plotting
 ti = seq(dti-dtw,0,dti) # for integrating (to be shifted)
 # shape funs
 tRR.funs = list(
- exp  = function(t,tsc=10,iRR=2){ tRR = 1+(t>0)*(iRR-1)*exp(-t/tsc) },
- ramp = function(t,tsc=30,iRR=2){ tRR = 1+(t>0)*(iRR-1)*pmax(0,1-t/tsc) },
- step = function(t,tsc=30,iRR=2){ tRR = 1+(t>0)*(iRR-1)*(t<=tsc) } )
+  exp  = function(t,tsc=10,iRR=2){ tRR = 1+(t>0)*(iRR-1)*exp(-t/tsc) },
+  ramp = function(t,tsc=30,iRR=2){ tRR = 1+(t>0)*(iRR-1)*pmax(0,1-t/tsc) },
+  step = function(t,tsc=30,iRR=2){ tRR = 1+(t>0)*(iRR-1)*(t<=tsc) })
+dRR.funs = list(
+  exp  = function(t,tsc=10,iRR=2){ tRR = (t<=0)+(t>0)*exp(-t/tsc) },
+  ramp = function(t,tsc=30,iRR=2){ tRR = (t<=0)+(t>0)*pmax(0,1-t/tsc) },
+  step = function(t,tsc=30,iRR=2){ tRR = (t<=0)+(t>0)*(t<=tsc) } )
 # integral function
 tRR.int = function(tRR.fun,te,tw){
   # te: event time, tw = end of window
   sum(tRR.fun(ti-te+tw)*dti/dtw)} # integrate over window & normalize
+tRR.adj = function(x){ c(1,x[1]-1+x[2],x[3:len(x)]) }
 # -----------------------------------------------------------------------------
 # integrate windows
-X.win = do.call(rbind,lapply(names(tRR.funs),function(shape){
-  tRR.fun = tRR.funs[[shape]]
-  tRR.w.all  = rowMeans(sapply(ti,function(tei){
-               sapply(tw,tRR.int,tRR.fun=tRR.fun,te=tei) }))
-  tRR.w.mean = sapply(tw,tRR.int,tRR.fun=tRR.fun,te=-dtw/2)
-  Xi = cbind(shape=shape,rbind(
-      data.frame(t=tw,tRR=tRR.w.mean,case='midpoint'),
-      data.frame(t=tw,tRR=tRR.w.all, case='complete') ))
-}))
-X.int = do.call(rbind,lapply(names(tRR.funs),function(shape){
-  tRR.fun = tRR.funs[[shape]]
-  Xi = merge(all=TRUE,cbind(i=0:nint),
-    data.frame(shape=shape,case='',t=c(0,tx),tRR=c(1,tRR.fun(tx))))
-}))
+xRR.win = function(xRR.funs){
+  X.win = do.call(rbind,lapply(names(xRR.funs),function(shape){
+    tRR.fun = xRR.funs[[shape]]
+    tRR.w.all  = rowMeans(sapply(ti,function(tei){
+                 sapply(tw,tRR.int,tRR.fun=tRR.fun,te=tei) }))
+    tRR.w.mean = sapply(tw,tRR.int,tRR.fun=tRR.fun,te=-dtw/2)
+    Xi = cbind(shape=shape,rbind(
+      data.frame(t=tw,case='Single*',tRR=tRR.adj(tRR.w.mean)),
+      data.frame(t=tw,case='Single', tRR=tRR.w.mean),
+      data.frame(t=tw,case='Double', tRR=tRR.w.all) ))
+}))}
+xRR.int = function(xRR.funs){
+  X.int = do.call(rbind,lapply(names(xRR.funs),function(shape){
+    tRR.fun = xRR.funs[[shape]]
+    Xi = merge(all=TRUE,cbind(i=0:nint),
+      data.frame(shape=shape,case='',t=c(0,tx),tRR=c(1,tRR.fun(tx))))
+}))}
 # -----------------------------------------------------------------------------
 # plot
-g = ggplot(X.win,aes(x=t,y=tRR,shape=case,color=case)) +
-  facet_grid('shape') +
-  geom_ribbon(data=X.int,aes(x=t+dtw*i/nint,ymin=1,ymax=tRR,fill=i,group=i),
-    alpha=2/nint,color=NA,show.legend=FALSE) +
-  geom_step(aes(lty=case)) +
-  scale_linetype_manual(name='Integral',values=c('solid','33')) +
-  scale_color_manual(name='Integral',values=c('purple','black','black')) +
-  scale_fill_distiller(palette='Spectral') +
-  scale_x_continuous(breaks=tw) +
-  labs(x='Time since event (days)',y='Transient Rate Ratio (tRR)') +
-  theme_light()
-g = plot.clean(g)
-plot.save('toy','tRR',str('tRR.int'),w=8,h=5)
-
+xRR.plot = function(X.win,X.int){
+  g = ggplot(X.win,aes(x=t-dtw/2,y=tRR,shape=case,color=case)) +
+    facet_grid('shape',scales='free_y',labeller=label_both) +
+    geom_hline(yintercept=1,color='gray') +
+    geom_ribbon(data=X.int,aes(x=t-dtw/2+dtw*i/nint,ymin=1,ymax=tRR,fill=i,group=i),
+      alpha=2/nint,color=NA,show.legend=FALSE) +
+    geom_step(aes(lty=case)) +
+    scale_linetype_manual(name='Integral',values=c('solid','62','22')) +
+    scale_color_manual(name='Integral',values=c('#ff00ff','#990099','#000000')) +
+    scale_fill_distiller(palette='Spectral') +
+    scale_x_continuous(breaks=tw) +
+    scale_y_continuous(breaks=seq(0,5,.5)) +
+    labs(x='Time since event timestep midpoint (days)')
+  g = plot.clean(g)
+}
+# -----------------------------------------------------------------------------
+# main
+for (f in c('t','d')){
+  xRR.funs = get(str(f,'RR.funs'))
+  g = xRR.plot(xRR.win(xRR.funs),xRR.int(xRR.funs)) +
+    ylab(str(list(t='Transient',d='Duration')[[f]],' Rate Ratio (',f,'RR)'))
+  plot.save('toy','tRR',str(f,'RR.int'),w=6,h=5)
+}
