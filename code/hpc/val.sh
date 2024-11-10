0661f3414b216edca3f7a3a40bf4c2be93b3fd77
jessexknight <jesse.x.knight@protonmail.com>
2024-11-10 14:06:49 +0000

    code: update hpc submit

--- code/hpc/val.sh
+++ code/hpc/val.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -j oe
 #PBS -N val
-#PBS -l walltime=01:30:00
+#PBS -l walltime=03:00:00
 #PBS -l select=1:ncpus=64:mem=512gb
 
 module add tools/prod
@@ -9,6 +9,6 @@ module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
 
-for v in {1..23}; do
+for v in {1..28}; do
   /bin/time -f "`cat hpc/time.fmt`" Rscript sim/val.r v=$v n.pop=1000 n.seed=64 .cores=64
 done
