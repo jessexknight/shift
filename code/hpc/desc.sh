173e6a0a070fc35756c5bf2dff3b51977c9267c6
jessexknight <jesse.x.knight@protonmail.com>
2025-02-17 15:43:08 +0000

    code: add desc/dep (big grid)

---
+++ code/hpc/desc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -N desc
+#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=64:mem=512gb
+
+module add tools/prod
+module add R/4.1.2-foss-2021b
+
+cd $PBS_O_WORKDIR
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/desc/dep.r .cores=64
