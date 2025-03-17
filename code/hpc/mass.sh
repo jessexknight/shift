47993d754c2db308688619a9d911fc39f9ece71d
jessexknight <jesse.x.knight@protonmail.com>
2025-03-17 10:14:56 +0000

    code: update hpc/mass

--- code/hpc/mass.sh
+++ code/hpc/mass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -j oe
 #PBS -N mass
-#PBS -l walltime=01:00:00
+#PBS -l walltime=02:30:00
 #PBS -l select=1:ncpus=64:mem=512gb
 
 module add tools/prod
@@ -9,7 +9,4 @@ module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
 
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/mass/dep.haz.r .cores=64 RR.step=0.2 Ro=.03 Rx=.03
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/mass/dep.haz.r .cores=64 RR.step=0.2 Ro=.01 Rx=.03
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/mass/dep.haz.r .cores=64 RR.step=0.2 Ro=.03 Rx=.01
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/mass/dep.haz.r .cores=64 RR.step=0.2 Ro=.01 Rx=.01
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/mass.dep.haz.r .cores=64
