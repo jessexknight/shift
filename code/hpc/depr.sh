29d7b4d67315f0c6ddcbfa1f741b904fee85d8c5
jessexknight <jesse.x.knight@protonmail.com>
2025-04-04 10:43:21 +0100

    code: update depr hpc

--- code/hpc/depr.sh
+++ code/hpc/depr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -j oe
-#PBS -N desc
+#PBS -N depr
 #PBS -l walltime=0:30:00
-#PBS -l select=1:ncpus=64:mem=512gb
+#PBS -l select=1:ncpus=128:mem=128gb
 
 module add tools/prod
 module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/desc/dep.r .cores=64
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/depr.r .cores=128
