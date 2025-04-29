2eda1a94999904c98abda2d776c2fa95e48bd78b
jessexknight <jesse.x.knight@protonmail.com>
2025-04-29 11:27:22 +0100

    code: split Y.all/Y.age in app/depr .rda data for speed

--- code/sim/app/depr.r
+++ code/sim/app/depr.r
@@ -44,6 +44,7 @@ P0 = list(
   RR.dep_o.dep_p = 1,
   dsc.dep_x.dep_u = Inf,
   run = get.run.par('dep',u=FALSE))
+t1y = add.pars.time(P0,P0$dtz)$t1y
 
 # -----------------------------------------------------------------------------
 # param grid & run sims
@@ -54,16 +55,17 @@ PG = list(
   dep.Ri.het      = c(0,.1,.2,.3,.5,1,2,3,5),
   dep.cov         = c(-.9,-.6,-.3, 0,+.3,+.6,+.9),
   RR.dep_o.dep_p  = 1 + c(0,.1,.2,.3,.5,1,2,3,5),
-  dsc.dep_x.dep_u = c(Inf,50,30,20,10,5,3,2,1)*360/log(2)
-)
+  dsc.dep_x.dep_u = c(Inf,50,30,20,10,5,3,2,1) * t1y / log(2))
 
 grid.path = function(p){
   hash.path(ulist(P0,PG[p]),'data','sim','depr',uid)
 }
 
 run.grid = function(p=NULL){
-  Y = fit.run.grid(PG[p],T,P0,srvs=srv.extra)
-  save.rda(Y,grid.path(p),'Y')
+  Y.age = fit.run.grid(PG[p],T,P0,srvs=srv.extra)
+  Y.all = subset(Y.age,!grepl(':',id))
+  save.rda(Y.age,grid.path(p),'Y.age')
+  save.rda(Y.all,grid.path(p),'Y.all')
 }
 
 load.grid = function(p=NULL,f=NULL){
