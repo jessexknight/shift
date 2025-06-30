75ff5023ea3934a5eb74f70f76ec6be2652c13d1
jessexknight <jesse.x.knight@protonmail.com>
2025-06-30 14:36:20 +0100

    code: update lhs hpc

--- code/hpc/lhs.sh
+++ code/hpc/lhs.sh
@@ -2,11 +2,11 @@
 #PBS -j oe
 #PBS -N lhs
 #PBS -l walltime=1:00:00
-#PBS -l select=1:ncpus=128:mem=128gb
-#PBS -J 1-9
+#PBS -l ncpus=16:mem=16gb
+#PBS -J 1-100
 
 module add tools/prod
 module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/lhs.r .cores=128 .nb=9 .b=$PBS_ARRAY_INDEX
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/lhs.r .cores=16 .nb=100 .b=$PBS_ARRAY_INDEX
