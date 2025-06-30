e8d0961aa7a2ba00aeb1cc5e4e66fa74c7cc77f8
jessexknight <jesse.x.knight@protonmail.com>
2025-06-30 15:36:48 +0100

    code: tweak hpc config

--- code/hpc/lhs.sh
+++ code/hpc/lhs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -j oe
 #PBS -N lhs
-#PBS -l walltime=1:00:00
+#PBS -l walltime=0:30:00
 #PBS -l select=1:ncpus=16:mem=16gb
 #PBS -J 1-100
 
@@ -9,4 +9,4 @@ module add tools/prod
 module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/lhs.r .cores=16 .nb=100 .b=$PBS_ARRAY_INDEX
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/lhs.dh.r .cores=16 .nb=100 .b=$PBS_ARRAY_INDEX
