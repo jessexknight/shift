8cae325e30e9185e5da16a82f16a7e3a12b5834b
jessexknight <jesse.x.knight@protonmail.com>
2025-07-29 15:07:52 +0100

    code: tweak cseb hpc config

--- code/hpc/cseb.sh
+++ code/hpc/cseb.sh
@@ -2,11 +2,11 @@
 #PBS -j oe
 #PBS -N cseb.o6
 #PBS -l walltime=0:45:00
-#PBS -l select=1:ncpus=128:mem=512gb
-#PBS -J 1-10
+#PBS -l select=1:ncpus=16:mem=64gb
+#PBS -J 1-100
 
 module add tools/prod
 module add R/4.1.2-foss-2021b
 
 cd $PBS_O_WORKDIR
-/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/cseb.r .k=o6 .cores=128 .nb=10 .b=$PBS_ARRAY_INDEX
+/bin/time -f "`cat hpc/time.fmt`" Rscript sim/app/cseb.r .k=o6 .cores=16 .nb=100 .b=$PBS_ARRAY_INDEX
