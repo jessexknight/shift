839ddfc181199248faee1e80c8e3a392a230a806
jessexknight <jesse.x.knight@protonmail.com>
2026-04-04 18:06:34 -0400

    code: add args to hpc/gen.sh

--- code/hpc/gen.sh
+++ code/hpc/gen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # inputs -----------------------------------------
-   HPC='hpc/mass.sh'
-SCRIPT='sim/app/mass.r'
-  NAME='RR2.ad.base'
- FLAGS='.debug=0 .k=RR2.ad.base'
-    NR=500    # num runs (total)
+   HPC="hpc/mass.sh"
+SCRIPT="sim/app/mass.r"
+  NAME=$1
+ FLAGS=".debug=0 .k=$1"
+    NR=$2     # num runs (total)
    TPR=30     # time per run (sec)
    MPR=2      # mem per run (GB)
     NB=100    # num batches
